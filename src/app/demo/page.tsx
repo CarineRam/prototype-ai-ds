8f1279b881904ff3cf271bfa8026492c8e9dfcd2
Carine Ramandimby <carineramandimby@gmail.com>
2024-05-02 16:53:46 +0300

    interface of prototype

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+// import Demo from "@/views/Demo"
+
+// type Props = {}{}: Props
+
+function page() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import '../styles/App.css'
+import Demo from '../views/Demo/index.tsx'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Demo/>
+      
+    </>
+  )
+}
+
+export default App
